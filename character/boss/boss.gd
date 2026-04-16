4d54148c2e8d6757a87417e5061a5f0a988054a4
Mikul-xyz <fmikulski@o2.pl>
2026-04-16 23:29:42 +0200

    boss atacks

---
+++ character/boss/boss.gd
@@ -0,0 +1,46 @@
+extends Node2D
+class_name Boss
+
+signal atack_signal
+
+@export_category("nodes")
+@export var canon_left: Node2D
+@export var canon_right: Node2D
+@export var head: Node2D
+
+
+@export_category("Single Atacks")
+@export var health: float = 1000
+@export var fase_health_points: Array[float] = [
+	0
+]
+
+@export_category("Atack Sets")
+@export var fase_1_atack_set: Array[Node]
+
+
+
+var current_atack_set: Array[Node]
+
+
+
+func _ready() -> void:
+	current_atack_set = fase_1_atack_set
+
+
+func take_damage(damage: float) -> void:
+	health -= damage
+	if health <= 0:
+		pass # TO DO WIN!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+func _on_atack_signal() -> void:
+	random_atack()
+
+
+func random_atack() -> void:
+	var atack = current_atack_set.pick_random()
+	if atack == null:
+		print("NO ATACK IN SET")
+	else:
+		atack.use()
