ff1ae9cfecca258bcb89855b194b38fd4db65776
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-04-17 01:40:31 +0200

    fix

--- characters/bodies/movement_components/grounded_movement_component.gd
+++ characters/bodies/movement_components/grounded_movement_component.gd
@@ -1,14 +1,17 @@
 class_name GroundedMovementComponent
 extends Node
 
-@export var speed: float = 300.0
+@export var max_speed: float = 300.0
+@export var acceleration: float = 100.0
 
 var body: BodyController
 
 func apply(_delta: float) -> void:
 	var movement_direction = Input.get_axis("move_left", "move_right")
 
-	body.velocity.x = movement_direction * speed 
+	var vel_change = movement_direction * acceleration 
+
+	body.velocity.x = clamp(body.velocity.x + vel_change, -max_speed, max_speed)
 
 
 
