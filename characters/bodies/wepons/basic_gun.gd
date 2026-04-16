b3c1e6a5d6ed221c2830a144427ae1680fc1b241
Mikul-xyz <fmikulski@o2.pl>
2026-04-17 01:56:57 +0200

    coldown

--- characters/bodies/wepons/basic_gun.gd
+++ characters/bodies/wepons/basic_gun.gd
@@ -5,6 +5,13 @@ class_name Gun
 @export var bullet: PackedScene
 @export var start: Node2D
 @export var bullet_speed: float = 1000
+@export var cooldown: float = 2
+
+var on_cooldown: float = 0
+
+func _process(delta: float) -> void:
+	if on_cooldown > 0:
+		on_cooldown -= delta
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -12,8 +19,12 @@ func _input(event: InputEvent) -> void:
 		rotation = Vector2.RIGHT.angle_to((mouse_position - global_position).normalized()) 
 		pass
 	if Input.is_action_just_pressed("shot"):
-		var x : Bullet = bullet.instantiate()
-		get_tree().current_scene.add_child(x)
-		x.global_position = start.global_position
-		
-		x.apply_central_impulse(Vector2.RIGHT.rotated(rotation) * bullet_speed)
+		if on_cooldown <= 0:
+			var x : Bullet = bullet.instantiate()
+			get_tree().current_scene.add_child(x)
+			x.global_position = start.global_position
+			x.apply_central_impulse(
+				Vector2.RIGHT.rotated(rotation) *
+				bullet_speed
+			)
+			on_cooldown = cooldown
