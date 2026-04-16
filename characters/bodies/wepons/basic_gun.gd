93feea01ecacd0538bc47fdd12d038b4673e9917
Mikul-xyz <fmikulski@o2.pl>
2026-04-17 01:49:33 +0200

    bullet

--- characters/bodies/wepons/basic_gun.gd
+++ characters/bodies/wepons/basic_gun.gd
@@ -2,11 +2,18 @@ extends Node2D
 
 class_name Gun
 
+@export var bullet: PackedScene
+@export var start: Node2D
+@export var bullet_speed: float = 1000
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var mouse_position = get_global_mouse_position()
 		rotation = Vector2.RIGHT.angle_to((mouse_position - global_position).normalized()) 
 		pass
 	if Input.is_action_just_pressed("shot"):
-		print("shot")
-		pass
+		var x : Bullet = bullet.instantiate()
+		get_tree().current_scene.add_child(x)
+		x.global_position = start.global_position
+		
+		x.apply_central_impulse(Vector2.RIGHT.rotated(rotation) * bullet_speed)
