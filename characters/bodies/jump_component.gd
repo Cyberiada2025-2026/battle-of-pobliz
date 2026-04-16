fc6c65abc29dc099330faec068c4dd7c74a31ab6
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-04-16 23:30:20 +0200

    fix double jump

--- characters/bodies/jump_component.gd
+++ characters/bodies/jump_component.gd
@@ -11,7 +11,7 @@ func apply(_delta: float) -> void:
 	if body.is_on_floor():
 		_jump_count = 0
 
-	if Input.is_action_pressed("jump") and _jump_count < max_jump_count:
+	if Input.is_action_just_pressed("jump") and _jump_count < max_jump_count:
 		_jump_count += 1
 		body.velocity.y = -jump_velocity
 
