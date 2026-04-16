f639ca40db77ff94bdaf5260e8c5329324aa812f
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-04-17 01:16:06 +0200

    add camera interpolation on switch

--- managers/possession_manager.gd
+++ managers/possession_manager.gd
@@ -4,6 +4,7 @@ extends Node
 @export var current_body: BodyController
 @export var player_camera: Camera2D
 @export var ray_cast: RayCast2D
+@export var camera_transfer_time: float = 0.5
 
 func _ready() -> void:
 	posses_body(current_body)
@@ -11,18 +12,24 @@ func _ready() -> void:
 func posses_body(body: BodyController) -> void:
 	current_body = body
 	player_camera.reparent(current_body)
-	print("possessed ", current_body) 
+	move_camera()
 
 
 func _physics_process(delta: float) -> void:
+	handle_possession()
+
+func handle_possession() -> void:
 	if Input.is_action_just_released("possess"):
 		ray_cast.target_position = player_camera.get_global_mouse_position() - ray_cast.global_position
 		ray_cast.force_raycast_update()
-		prints(ray_cast.position, ray_cast.target_position)
 
 		var collider = ray_cast.get_collider() as BodyController
 		if collider == null:
-			print("possession failed")
 			return
 
 		posses_body(collider)
+
+
+func move_camera() -> void:
+	var position_tween = create_tween().set_trans(Tween.TRANS_CUBIC)
+	position_tween.tween_property(player_camera, "position", Vector2.ZERO, camera_transfer_time)
