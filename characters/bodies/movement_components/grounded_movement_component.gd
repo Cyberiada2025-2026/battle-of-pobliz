8e88023cd208617dc984197e999ded800ba2e7b1
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-04-17 01:41:49 +0200

    remove floaty movement

--- characters/bodies/movement_components/grounded_movement_component.gd
+++ characters/bodies/movement_components/grounded_movement_component.gd
@@ -1,17 +1,16 @@
 class_name GroundedMovementComponent
 extends Node
 
-@export var max_speed: float = 300.0
+@export var speed: float = 300.0
 @export var acceleration: float = 100.0
+@export var deceleration: float = 200.0
 
 var body: BodyController
 
 func apply(_delta: float) -> void:
 	var movement_direction = Input.get_axis("move_left", "move_right")
 
-	var vel_change = movement_direction * acceleration 
-
-	body.velocity.x = clamp(body.velocity.x + vel_change, -max_speed, max_speed)
+	body.velocity.x = movement_direction * speed 
 
 
 
