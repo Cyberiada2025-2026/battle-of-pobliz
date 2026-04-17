6394dfe3c6a5843f0599a0f80af4a6f8bcf71567
Mikul-xyz <fmikulski@o2.pl>
2026-04-17 17:42:59 +0200

    time

--- scenes/victory_screan/win_screen.gd
+++ scenes/victory_screan/win_screen.gd
@@ -1,11 +1,14 @@
 extends Control
 
 @export var empty_scene: PackedScene
+@export var time_label: Label
 
 func _ready() -> void:
 	MainMenu.activable = false
 	if MainMenu.visible == true:
 		MainMenu._on_resume_button_pressed()
+	var score: int = MainMenu.timer
+	time_label.text = "Time: "+ str(score/60) + ":" + str(score%60)
 
 
 func _on_main_menu_button_pressed() -> void:
