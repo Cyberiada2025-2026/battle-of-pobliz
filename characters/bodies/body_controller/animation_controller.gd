ab1df39873dea0e0522d5f9fd44074902a06fe86
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-04-17 09:47:49 +0200

    add animation controller

---
+++ characters/bodies/body_controller/animation_controller.gd
@@ -0,0 +1,26 @@
+class_name AnimationController
+extends Node
+
+@export var animated_sprite: AnimatedSprite2D
+@export var body: BodyController
+## key is an expression that evaluates to true when animation is supposed to be played
+## value is the name of the animation
+## earlier has precedence
+@export var animations: Array[ConditionalAnimation]
+
+func _process(_delta: float) -> void:
+	animated_sprite.flip_h = body.velocity.x < 0
+	for conditional_anim in animations:
+		var expression = Expression.new()
+		var animation_condition = conditional_anim.condition
+		var error = expression.parse(animation_condition)
+		if error != OK:
+			print(expression.get_error_text())
+			return
+		var result = expression.execute([], self)
+		if not expression.has_execute_failed():
+			if result and animated_sprite.animation != conditional_anim.animation_name:
+				animated_sprite.play(conditional_anim.animation_name)
+				return
+
+
--- characters/bodies/movement_components/jump_component.gd
+++ characters/bodies/movement_components/jump_component.gd
@@ -7,13 +7,21 @@ extends Node
 var body: BodyController
 var _extra_jump_count = 0
 
+var is_jumping: bool = false
+
 func apply(_delta: float) -> void:
 	if body.is_on_floor():
 		_extra_jump_count = 0
+		is_jumping = false
+
+	if body.velocity.y > 0:
+		is_jumping = false
 
 	if Input.is_action_just_pressed("jump"):
 		if _extra_jump_count < max_extra_jump_count:
 			_extra_jump_count += 1
 			body.velocity.y = -jump_velocity
+			is_jumping = true
 		elif body.is_on_floor():
 			body.velocity.y = -jump_velocity
+			is_jumping = true
