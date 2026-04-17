38e14c11d4f085929f07dc9a2136ae87f3f036fb
Titanic472 <ctac472@gmail.com>
2026-04-17 02:00:33 +0200

    working boss miniguns!!!

--- characters/boss/atacks/basic_cannon_shot/basic_cannon_attack.gd
+++ characters/boss/atacks/basic_cannon_shot/basic_cannon_attack.gd
@@ -15,22 +15,23 @@ var bullet_prefab = preload("res://shared/projectiles/boss/basic_bullet.tscn")
 
 
 func use():
+	var offset = randi_range(0, cannons.size())
 	for i in shot_series:
-		shot_serie(cannons[i % cannons.size()])
-
+		print("serie")
+		shot_serie(cannons[(i + offset) % cannons.size()])
 		await get_tree().create_timer(shot_series_cooldown).timeout
 
 
 func shot_serie(cannon: BossCanon):
+	print("first shot")
 	for i in shot_count:
 		var bullet: RigidBody2D = bullet_prefab.instantiate()
 		get_tree().current_scene.add_child(bullet)
-		var player_pos = Vector2(592.0, 416.0)
-		var move_direction = (player_pos - cannon.position).normalized()
+		var player = get_tree().get_first_node_in_group("possession_manager").current_body
+		var move_direction = (player.global_position - cannon.global_position).normalized()
 
 		bullet.apply_central_impulse(move_direction * bullet_speed)
 		bullet.global_position = cannon.global_position
-		print(bullet.position)
-		print(bullet.linear_velocity)
 
 		await get_tree().create_timer(shot_cooldown).timeout
+	print("last shot")
--- shared/projectiles/bullet.gd
+++ shared/projectiles/bullet.gd
@@ -3,6 +3,5 @@ extends RigidBody2D
 
 
 
-func _on_body_entered(body: Node) -> void:
-	print("collide")
+func _on_body_entered(_body: Node) -> void:
 	self.queue_free()
