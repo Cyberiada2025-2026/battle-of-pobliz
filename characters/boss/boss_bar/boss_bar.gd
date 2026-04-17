09787d0d28ab03514bb8eea3984b0ca0463e83bc
Mikul-xyz <fmikulski@o2.pl>
2026-04-17 10:09:58 +0200

    boss healthbar

--- characters/boss/boss_bar/boss_bar.gd
+++ characters/boss/boss_bar/boss_bar.gd
@@ -5,9 +5,27 @@ class_name BossBar
 @export var bar_base: Node
 
 var bars: Array[PhaseBar]
+var cur_bar: PhaseBar
 
 func _ready() -> void:
-	for phase in boss.phases:
+	var i: int = boss.phases.size() + 1
+	for phase in [boss.current_phase] + boss.phases:
 		var bar = PhaseBar.new()
 		bar_base.add_child(bar)
+		bars.append(bar)
 		bar.color = phase.bar_color
+		bar.phase = phase
+		bar.length = bar_base.size.x
+		bar.size.y = bar_base.size.y
+		bar.health = phase.phase_health
+		bar.set_health(phase.phase_health)
+		bar.z_index += i
+		i -= 1
+	cur_bar = bars.pop_front()
+
+func _process(_delta: float) -> void:
+	if cur_bar.phase != boss.current_phase:
+		cur_bar.queue_free()
+		print("dsfs")
+		cur_bar = bars.pop_front()
+	cur_bar.set_health(boss.health)
--- characters/boss/boss_bar/phasebar.gd
+++ characters/boss/boss_bar/phasebar.gd
@@ -1,2 +1,11 @@
 extends ColorRect
 class_name PhaseBar
+
+var health: float = 0
+var cur_health: float = 0
+var length: float = 0
+var phase: BossPhase
+
+func set_health(boss_health: float) -> void:
+	cur_health = boss_health
+	size.x = length * (cur_health/health)
