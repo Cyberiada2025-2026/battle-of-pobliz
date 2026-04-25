44adcb2106fae040ab185a57424de3880c1ab87d
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-04-25 10:52:57 +0200

    add coyote time

--- characters/bodies/movement_components/jump_component.gd
+++ characters/bodies/movement_components/jump_component.gd
@@ -7,13 +7,18 @@ extends Node
 @export var jump_vfx: GPUParticles2D
 @export var land_vfx: GPUParticles2D
 
+@export_group("Jump Buffer")
 @export var jump_buffer_time: float = 0.1
 @export var jump_buffer_ray: RayCast2D
+
+@export_group("Coyote Time")
+@export var coyote_time: float = 0.1
 
 var body: BodyController
 
 var wants_jump: bool = false
 var is_jumping: bool = false
+var was_on_floor: bool = false
 var landed: bool = true
 
 var _extra_jump_count = 0
@@ -28,9 +33,15 @@ func handle_input(delta: float) -> void:
 	if _wants_jump_timer > jump_buffer_time:
 		wants_jump = false
 
+func handle_floor() -> void:
+	if body.is_on_floor():
+		was_on_floor = true
+		get_tree().create_timer(coyote_time).timeout.connect(func(): was_on_floor = false)
+
 
 func apply(delta: float) -> void:
 	handle_input(delta)
+	handle_floor()
 	if body.is_on_floor():
 		_extra_jump_count = 0
 		is_jumping = false
@@ -45,13 +56,15 @@ func apply(delta: float) -> void:
 		landed = false
 
 	if wants_jump:
-		if body.is_on_floor():
+		if was_on_floor:
+			print("jumped")
 			wants_jump = false
 			body.velocity.y = -jump_velocity
 			is_jumping = true
 			jump_vfx.restart()
 			jump_vfx.emitting = true
 		elif _extra_jump_count < max_extra_jump_count and not jump_buffer_ray.is_colliding():
+			print("double jumped")
 			wants_jump = false
 			_extra_jump_count += 1
 			is_jumping = true
